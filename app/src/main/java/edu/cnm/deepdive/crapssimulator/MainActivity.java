6ce84a8680d32aefb9b6903342d86e6f10430415
JustinBDominguez <justinbdominguez1990@gmail.com>
2019-09-23 16:45:32 -0600

    Added ListVIew and tally TextView with dummy data.

--- app/src/main/java/edu/cnm/deepdive/crapssimulator/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/crapssimulator/MainActivity.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive.crapssimulator;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    TextView tally  = findViewById(R.id.tally);
+    tally.setText("This will be my win/loss tally line");
+    ListView rolls = findViewById(R.id.rolls);
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+        getResources().getStringArray(R.array.dummy_rolls));
+    rolls.setAdapter(adapter);
   }
 
   @Override
@@ -26,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
     boolean handled = true;
     switch (item.getItemId()) {
       case R.id.run:
-        Toast.makeText(this, getString(R.string.run_toast), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.run_toast, Toast.LENGTH_LONG).show();
         break;
       case R.id.reset:
-        Toast.makeText(this, getString(R.string.reset_toast), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.reset_toast, Toast.LENGTH_LONG).show();
         break;
       default:
         handled = super.onOptionsItemSelected(item);
